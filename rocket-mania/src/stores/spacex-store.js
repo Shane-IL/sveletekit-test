d60c9e3536c685193ba3a6941017f1d44da99c5b
Shane <shanegoodson101@gmail.com>
2021-09-04 16:57:28 +0300

    Added cards to display items and integrated data store

--- rocket-mania/src/stores/spacex-store.js
+++ rocket-mania/src/stores/spacex-store.js
@@ -2,12 +2,9 @@ import { writable } from "svelte/store";
 
 export const spacexRockets = writable([]);
 
-const fetchAllRockets = async () => {
+export const fetchAllRockets = async () => {
     const url = 'https://api.spacexdata.com/v4/rockets';
     const res = await fetch(url);
     const data = await res.json();
-    const dataAsArr = Object.values(data);
-    spacexRockets.set(dataAsArr);
+    spacexRockets.set(data);
 }
-
-fetchAllRockets();
