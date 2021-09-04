fcc4619245071753a0705ea5219fe9dbf7cc6d86
Shane <shanegoodson101@gmail.com>
2021-09-04 20:23:57 +0300

    styled rocket view

--- src/routes/rocket/[id].svelte
+++ src/routes/rocket/[id].svelte
@@ -12,18 +12,18 @@
 	const rocketDetails = $spacexRockets.find((rocket) => rocket.id === rocketId);
 </script>
 
-<div>
-	<div>{rocketDetails.name}</div>
-	<img alt={rocketDetails.name} src={rocketDetails.flickr_images[0]} />
-	<p>{rocketDetails.description}</p>
-	<div>Dimentions</div>
-	<ul>
+<div class="w-8/12 text-left m-auto">
+	<div class="text-6xl font-semibold text-center">{rocketDetails.name}</div>
+	<img class="my-6 h-96 m-auto" alt={rocketDetails.name} src={rocketDetails.flickr_images[0]} />
+	<p class="text-lg">{rocketDetails.description}</p>
+	<div class="my-4 text-2xl">Dimentions</div>
+	<ul class="leading-8">
 		<li><b>Height: </b>{rocketDetails.height.meters}m</li>
 		<li><b>Diameter: </b>{rocketDetails.diameter.meters}m</li>
 		<li><b>Mass: </b>{rocketDetails.mass.kg}kg</li>
 	</ul>
-	<div>Details</div>
-	<ul>
+	<div class="my-4 text-2xl">Details</div>
+	<ul class="leading-8">
 		<li><b>Stages: </b>{rocketDetails.stages}</li>
 		<li><b>First Flight: </b>{rocketDetails.first_flight}</li>
 		<li><b>Launch Cost: </b>${rocketDetails.cost_per_launch}</li>
