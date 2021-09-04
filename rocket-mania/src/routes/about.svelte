a07ccd55c0baca74eac792dfbcbb0a296b168226
Shane <shanegoodson101@gmail.com>
2021-09-04 17:02:02 +0300

    expanded about page

--- rocket-mania/src/routes/about.svelte
+++ rocket-mania/src/routes/about.svelte
@@ -4,5 +4,18 @@
 <h1>About</h1>
 <p>
 	This is a small personal project for me to get acquinted with SvelteKit<br />It will use some open
-	APIs to display data about space rockets
+	APIs to display data about space rockets. I recently made a large webapp for work using NextJS and
+	at the same time started to get into Svelte and quite liked the concept of SvelteKit so here we
+	go.
 </p>
+
+<h3>Technologies used:</h3>
+<ul>
+	<li>SvelteKit (obviously)</li>
+	<li>Tailwind CSS</li>
+</ul>
+
+<h3>Data sourced from:</h3>
+<ul>
+	<li><a href="https://github.com/r-spacex/SpaceX-API">r/Spacex open API</a></li>
+</ul>
