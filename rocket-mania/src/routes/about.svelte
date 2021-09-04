dd100127a433a464b30e85e93c20939352dd262b
Shane <shanegoodson101@gmail.com>
2021-09-04 16:20:09 +0300

    Basic setup

---
+++ rocket-mania/src/routes/about.svelte
@@ -0,0 +1,8 @@
+<svelte:head>
+	<title>Rocket-Mania - About</title>
+</svelte:head>
+<h1>About</h1>
+<p>
+	This is a small personal project for me to get acquinted with SvelteKit<br />It will use some open
+	APIs to display data about space rockets
+</p>
